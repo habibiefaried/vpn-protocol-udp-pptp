dfbd01fc3a443a52f24edf11c4e737eaa31fcca4
Habibie Faried <habibiefaried@gmail.com>
2022-04-03 19:17:16 +0400

    feat: simple encryption

--- Makefile
+++ Makefile
@@ -2,3 +2,4 @@ integration-test:
 	go fmt ./...
 	go build .
 	go test -parallel 4 -v integration_test.go
+	go test -parallel 4 -v encrypt.go encrypt_test.go
---
+++ encrypt.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"errors"
+	"golang.org/x/crypto/chacha20poly1305"
+)
+
+func encrypt(plaintext, pass string) ([]byte, error) {
+	key := sha256.Sum256([]byte(pass))
+	aead, err := chacha20poly1305.NewX(key[:])
+	if err != nil {
+		return nil, err
+	}
+
+	msg := []byte(plaintext)
+
+	nonce := make([]byte, aead.NonceSize(), aead.NonceSize()+len(msg)+aead.Overhead())
+	if _, err := rand.Read(nonce); err != nil {
+		return nil, err
+	}
+
+	return aead.Seal(nonce, nonce, msg, nil), nil
+}
+
+func decrypt(encryptedMsg []byte, pass string) ([]byte, error) {
+	key := sha256.Sum256([]byte(pass))
+	aead, err := chacha20poly1305.NewX(key[:])
+	if err != nil {
+		return nil, err
+	}
+
+	if len(encryptedMsg) < aead.NonceSize() {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	// Split nonce and ciphertext.
+	nonce, ciphertext := encryptedMsg[:aead.NonceSize()], encryptedMsg[aead.NonceSize():]
+
+	// Decrypt the message and check it wasn't tampered with.
+	plaintext, err := aead.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
